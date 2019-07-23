1ba930ac2ec1c2b09d2817453c9ac5d65d8e6930
Brian Sherrell <brsherr@microsoft.com>
2019-07-23 14:15:12 -0700

    Add project files.

---
+++ Py4EAssignment1/Py4EAssignment1.py
@@ -0,0 +1,11 @@
+# Project Py4E
+# Assignment 1
+# Objective: Print out Hello World
+# Author: Brian Sherrell
+# email: jsocdev@gmx.com / brsherr@microsoft.com
+
+# Display something other than Hello World
+print('Hey Lewis, nice win at Silverstone Sunday!')
+
+# End Program
+quit()
